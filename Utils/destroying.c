a9268947d6c06ff6631ac87d02e2f7aaa9d4ce3d
Tiberio Bezerra Soares <tbezerra@c1r2s7.42porto.com>
2025-04-02 18:31:14 +0100

    Memory leak

--- Utils/destroying.c
+++ Utils/destroying.c
@@ -6,7 +6,7 @@
 /*   By: tbezerra <tbezerra@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 19:30:33 by joao-rib          #+#    #+#             */
-/*   Updated: 2025/04/02 17:05:16 by tbezerra         ###   ########.fr       */
+/*   Updated: 2025/04/02 18:27:30 by tbezerra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,21 @@ static void	destroy_img(t_game *g)
 {
 	int	i;
 
+	if (!g->texture_img)
+		return;
 	i = 0;
 	while (i < 4)
 	{
-		if (g->texture_img[i] && g->texture_img[i]->img)
+		if (g->texture_img[i])
 		{
-			mlx_destroy_image(g->mlx_ptr, g->texture_img[i]->img); // Libera a imagem
-			free(g->texture_img[i]); 
+			if (g->texture_img[i]->img)
+				free(g->texture_img[i]->img); // Libera `t_graph`
+			free(g->texture_img[i]); // Libera `t_text_img`
 		}
-/* 			free(g->texture_img[i]->img); //WIP Verificar addr (valgrind)
-		free(g->texture_img[i]); */
 		i++;
 	}
-	free(g->texture_img);
+	free(g->texture_img); // Libera o array de ponteiros
+	g->texture_img = NULL;
 }
 
 void	destroy_map(t_game *g)
@@ -63,6 +65,10 @@ int	destroy_game(t_game *g)
 		return (1);
 	if (g->ray)
 		free(g->ray);
+	if (g->texture_img)
+		destroy_img(g);
+	if (g->display->mlx_img)
+		mlx_destroy_image(g->mlx_ptr, g->display->mlx_img);
 	if (g->display)
 		free(g->display);
 /* 	{
@@ -73,9 +79,10 @@ int	destroy_game(t_game *g)
 	if (g->win_ptr)
 		mlx_destroy_window(g->mlx_ptr, g->win_ptr);
 	if (g->mlx_ptr)
+	{
 		mlx_destroy_display(g->mlx_ptr);
-	if (g->texture_img)
-		destroy_img(g);
+		free(g->mlx_ptr);
+	}
 	if (g->map)
 		destroy_map(g);
 	exit(1);
