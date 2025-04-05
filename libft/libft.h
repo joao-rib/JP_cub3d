630f6e0debb036edbc9e494eb8c639b6e0f2a71d
Tiberio Bezerra Soares <tbezerra@c1r2s7.42porto.com>
2025-04-05 17:02:57 +0100

    Bug find

--- libft/libft.h
+++ libft/libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joao-rib <joao-rib@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tbezerra <tbezerra@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 19:30:33 by joao-rib          #+#    #+#             */
-/*   Updated: 2025/04/05 12:18:48 by joao-rib         ###   ########.fr       */
+/*   Updated: 2025/04/05 15:57:56 by tbezerra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@
 # define HEXA_UPP "0123456789ABCDEF"
 //get_next_line
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
+#  define BUFFER_SIZE 1000000
 # endif
 # define MAXFD 2000
 # define FOPEN_MAX 16
